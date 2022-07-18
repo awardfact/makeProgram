97a2f879838569dc10685ccf9e8853657b96aa63
unknown <awardfact@hanmail.net>
2022-07-18 22:08:40 +0900

    '0718'

--- node/bookSource/index.js
+++ node/bookSource/index.js
@@ -1,15 +1,3 @@
-const { hol , jjak } = require("../var.js");
-const checkNumber = require("../func.js");
-
-
-function checkString(str){
-    if(str.length % 2){
-        return hol;
-    }else{
-        return jjak;
-    }
+module.exports = () =>{
+    return '패키지 리턴';
 }
-
-
-console.log(checkNumber(10));
-console.log(checkString('hello'));
